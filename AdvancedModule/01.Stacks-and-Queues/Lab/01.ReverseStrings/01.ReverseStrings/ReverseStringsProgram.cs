4abddc96aac8d9106ec2358232f572a1eb59a856
StefanYankov <syankoff@gmail.com>
2019-07-10 17:50:29 +0300

    Lab tasks 01 and 02

---
+++ AdvancedModule/01.Stacks-and-Queues/Lab/01.ReverseStrings/01.ReverseStrings/ReverseStringsProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ReverseStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            var reversingStack = new Stack<char>();
+
+            foreach (var item in input)
+            {
+                reversingStack.Push(item);
+            }
+
+
+            while (reversingStack.Count > 0)
+            {
+                Console.Write(reversingStack.Peek());
+                reversingStack.Pop();
+            }
+            Console.WriteLine();
+        }
+    }
+}
