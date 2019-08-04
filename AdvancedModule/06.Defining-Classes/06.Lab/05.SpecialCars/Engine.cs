2f632b905d529dbfc4c61c7fbc60bd7ff3a31195
StefanYankov <40911775+StefanYankov@users.noreply.github.com>
2019-08-04 21:44:24 +0300

    Lab

---
+++ AdvancedModule/06.Defining-Classes/06.Lab/05.SpecialCars/Engine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.SpecialCars
+{
+    public class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+
+        public int HorsePower
+        {
+            get { return this.horsePower; }
+            set { this.horsePower = value; }
+        }
+
+        public double CubicCapacity
+        {
+            get { return this.cubicCapacity; }
+            set { this.cubicCapacity = value; }
+        }
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+    }
+}
