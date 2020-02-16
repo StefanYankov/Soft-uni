9ca9f8d909db59fc2356474e8a87d0f828dff7ea
StefanYankov <40911775+StefanYankov@users.noreply.github.com>
2020-02-16 12:58:53 +0200

    3/7 Problems solved

---
+++ C#/AdvancedModule/09.Iterators-and-Comparators/09.Exercise/03Stack/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _03Stack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CustomStack<int> myCustomStack = new CustomStack<int>();
+
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] tokens = input
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string command = tokens[0];
+
+                if (command == "Push")
+                {
+                    for (int currentNumber = 1; currentNumber < tokens.Length; currentNumber++)
+                    {
+                        myCustomStack.Push(int.Parse(tokens[currentNumber]));
+                    }
+                }
+                else if (command == "Pop")
+                {
+                    myCustomStack.Pop();
+                }
+            }
+
+            foreach (var number in myCustomStack)
+            {
+                Console.WriteLine(number);
+            }
+
+            foreach (var number in myCustomStack)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
