6389f077860dfd46dd9af3c96d0ec13dd3c6f627
StefanYankov <40911775+StefanYankov@users.noreply.github.com>
2020-02-16 12:58:44 +0200

    4/7 Problems solved

---
+++ C#/AdvancedModule/09.Iterators-and-Comparators/09.Exercise/04Froggy/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04Froggy
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<int> stones = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+            Lake lake = new Lake(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+
+        }
+    }
+}
