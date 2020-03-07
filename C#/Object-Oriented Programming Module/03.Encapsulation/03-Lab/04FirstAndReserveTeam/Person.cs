0aaa08353dd95de091d8d02640a51d5667d49493
StefanYankov <40911775+StefanYankov@users.noreply.github.com>
2020-03-07 20:57:11 +0200

    4/4 Lab problems solved

---
+++ C#/Object-Oriented Programming Module/03.Encapsulation/03-Lab/04FirstAndReserveTeam/Person.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.");
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                percentage = percentage / 2;
+            }
+
+            this.Salary *= 1 + (percentage / 100.0M);
+        }
+    }
+}
---
+++ C#/Object-Oriented Programming Module/03.Encapsulation/03-Lab/04FirstAndReserveTeam/Team.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private List<Person> firstSquad;
+        private List<Person> reserveSquad;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            this.firstSquad = new List<Person>();
+            this.reserveSquad = new List<Person>();
+        }
+
+        public string Name { get; }
+
+        public IReadOnlyList<Person> FirstSquad => this.firstSquad.AsReadOnly();
+
+        public IReadOnlyList<Person> ReserveSquad
+        {
+            get
+            {
+                return this.reserveSquad.AsReadOnly();
+            }
+        }
+
+        public void AddPlayer(Person player)
+        {
+            if (player.Age < 40)
+            {
+                this.firstSquad.Add(player);
+            }
+            else
+            {
+                this.reserveSquad.Add(player);
+            }
+        }
+    }
+}
