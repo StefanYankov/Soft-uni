40cdbd70baae0704ba421cca2c1367fca9ab8f8f
StefanYankov <40911775+StefanYankov@users.noreply.github.com>
2020-03-15 21:47:29 +0200

    3/3 solved problems

---
+++ C#/Object-Oriented Programming Module/05.Polymorphism/05-Lab/03Shapes/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius
+        {
+            get => this.radius;
+            private set
+            {
+                this.radius = value;
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            double area = Math.PI * this.Radius * this.Radius;
+
+            return area;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            double perimeter = 2 * Math.PI * this.Radius;
+
+            return perimeter;
+        }
+
+        public sealed override string Draw()
+        {
+            return base.Draw() + "Circle";
+        }
+    }
+}
---
+++ C#/Object-Oriented Programming Module/05.Polymorphism/05-Lab/03Shapes/Rectangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private int height;
+        private int width;
+
+        public Rectangle(int height, int width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public int Height
+        {
+            get => this.height;
+            private set
+            {
+
+                this.height = value;
+            }
+        }
+
+        public int Width
+        {
+            get => this.width;
+            private set
+            {
+                this.width = value;
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            double area = this.Width * this.Height;
+
+            return area;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            double perimeter = 2 * (this.Width + this.Height);
+
+            return perimeter;
+        }
+
+        public sealed override string Draw()
+        {
+            return base.Draw() + "Rectangle";
+        }
+    }
+}
