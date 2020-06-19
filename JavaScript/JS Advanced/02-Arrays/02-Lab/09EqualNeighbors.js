4fd3fd9fb632d8ea7b1b76005578b1f63b2b04c4
StefanYankov <40911775+StefanYankov@users.noreply.github.com>
2020-06-19 21:33:42 +0300

    added a second solve with .reduce

--- JavaScript/JS Advanced/02-Arrays/02-Lab/09EqualNeighbors.js
+++ JavaScript/JS Advanced/02-Arrays/02-Lab/09EqualNeighbors.js
@@ -1,3 +1,56 @@
+// with for loops
+const matrix = [
+  ['2', '3', '4', '7', '0'],
+  ['4', '0', '5', '3', '4'],
+  ['2', '8', '1', '4', '2'],
+  ['9', '8', '7', '5', '4']
+];
+
+const stringMatrix = [
+  ['test', 'yes', 'yo', 'ho'],
+  ['well', 'done', 'yo', '6'],
+  ['not', 'done', 'yet', '5']
+];
+
+const thirdMatrix = [
+  [2, 2, 5, 7, 4],
+  [4, 0, 5, 3, 4],
+  [2, 5, 5, 4, 2],
+];
+function getNeightboursCount(inputMatrix) {
+
+  let count = 0
+  for (let i = 0; i < inputMatrix.length; i++) {
+
+      const currentElement = inputMatrix[i];
+      const nextElement = inputMatrix[i + 1];
+
+      for (let j = 0; j < currentElement.length; j++) {
+
+          let firstElement = currentElement[j];
+
+          
+          if (j < currentElement.length - 1) {
+              let secondElement = currentElement[j + 1];
+              if (firstElement === secondElement) {
+                  count++;
+              }
+          }
+
+          if(i < inputMatrix.length - 1) {
+              let firstElementNextRow = nextElement[j];
+              if (firstElement === firstElementNextRow) {
+                  count++;
+              }
+          }
+      }
+  }
+  return count;
+}
+console.log(getNeightboursCount(thirdMatrix));
+
+
+// with reduce
 function equalNeighbors(input2DArray) {
   const result = input2DArray.reduce((acc, currRow, rowIndex) => {
     const currCount = currRow.reduce((acc, currValue, currIndex) => {
