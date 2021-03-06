84288e2483f92f023dca64d6b675968c23d41eb4
Stefan Yankov <40911775+StefanYankov@users.noreply.github.com>
2021-03-06 11:36:53 +0200

    Delete AI directory

--- AI/Python/Programming Basics with Python/5. Loops/min_number.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-min_number = 9223372036854775807
-
-for number in range (1, n+1):
-    input_number = int(input())
-    if input_number < min_number:
-        min_number = input_number
-
-print(min_number)
