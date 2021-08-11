f202f8c29abd20cf3c2d21c7c8c3a867c89336f9
Stefan Yankov <40911775+StefanYankov@users.noreply.github.com>
2021-08-11 12:33:38 +0300

    Add files via upload

---
+++ C#/Fundamentals Module/09-Regular-Expressions/09-More-Exercise/P02RageQuit/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P02RageQuit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    public class Program
+    {
+        public static void Main()
+        {
+            HashSet<char> uniqueChars = new HashSet<char>();
+            StringBuilder result = new StringBuilder();
+            string pattern = @"(?<text>[\D]+)(?<number>[\d]+)";
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match item in matches)
+            {
+                string text = item.Groups["text"].Value.ToUpper();
+                int number = int.Parse(item.Groups["number"].Value);
+
+                if (number > 0)
+                {
+                    foreach (var charLetter in text)
+                    {
+                        uniqueChars.Add(charLetter);
+                    }
+                }
+
+                for (int i = 0; i < number; i++)
+                {
+                    result.Append(text);
+                }
+            }
+
+            Console.WriteLine($"Unique symbols used: {uniqueChars.Count}");
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ C#/Fundamentals Module/09-Regular-Expressions/09-More-Exercise/P04Santa'sSecretHelper/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P04Santa_sSecretHelper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    public class Program
+    {
+        public static void Main()
+        {
+            int key = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            string pattern = @"@(?<name>[A-Za-z]+)[^@\\\-!:>]*!([G])!";
+            List<string> children = new List<string>();
+
+            while (!(input = Console.ReadLine()).Equals("end"))
+            {
+                var charArray = input.ToCharArray();
+
+                for (int i = 0; i < charArray.Length; i++)
+                {
+                    charArray[i] = (char)(charArray[i] - key);
+                }
+
+                input = new string(charArray);
+                MatchCollection matches = Regex.Matches(input, pattern);
+                if (matches.Count > 0)
+                {
+                    foreach (Match match in matches)
+                    {
+                        children.Add(match.Groups["name"].Value);
+                    }
+                }
+            }
+
+            foreach (var child in children)
+            {
+                Console.WriteLine(child);
+            }
+        }
+    }
+}
