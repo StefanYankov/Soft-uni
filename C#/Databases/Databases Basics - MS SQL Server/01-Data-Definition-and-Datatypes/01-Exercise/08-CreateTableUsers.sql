231ded948d0c885a63a68bb3de8a0442e0bfe62b
Stefan Yankov <40911775+StefanYankov@users.noreply.github.com>
2021-10-03 10:47:37 +0300

    Updates
    adding Unique to Username
    Updating data types for LastLoginTime from DATETIME to DATETIME2
    Flagging [IsDeleted] that it can't be null

--- C#/Databases/Databases Basics - MS SQL Server/01-Data-Definition-and-Datatypes/01-Exercise/08-CreateTableUsers.sql
+++ C#/Databases/Databases Basics - MS SQL Server/01-Data-Definition-and-Datatypes/01-Exercise/08-CreateTableUsers.sql
@@ -2,23 +2,22 @@
 USE Minions
 
 CREATE TABLE Users (
-	Id BIGINT PRIMARY KEY IDENTITY(1,1),
-	Username VARCHAR(30) NOT NULL,
+	[Id] BIGINT PRIMARY KEY IDENTITY(1,1),
+	[Username] VARCHAR(30) UNIQUE NOT NULL,
 	[Password] VARCHAR(26) NOT NULL,
-	ProfilePicture VARBINARY(max),
+	[ProfilePicture] VARBINARY(max),
     CHECK(DATALENGTH(ProfilePicture) <= 900000),
-	LastLoginTime DATETIME,
-	IsDeleted BIT
+	[LastLoginTime] DATETIME2,
+	[IsDeleted] BIT NOT NULL,
 )
 
 SELECT * FROM Users
 
-INSERT INTO Users(Username, [Password], ProfilePicture, LastLoginTime, IsDeleted) VALUES
+INSERT INTO Users([Username], [Password], [ProfilePicture], [LastLoginTime], [IsDeleted]) VALUES
 ('Stefan', 'yPQC5Y8zTSqLhHxC',100, '2020-01-02 12:00', 0),
 ('Serafim', '2g9RM5pq8JuRQHSM', 200, '2020-03-04 12:01', 0),
 ('Dala', '9yJRa8WD2nz2tNfH', 300, '2020-05-06 12:02', 1),
 ('Kaka', 'EG9Rwa7Ccds3RbGd', 400, '2020-07-08 12:03', 0),
 ('Spaska', 'VF9nFnzAnkPrq5gY', 500, '2020-09-10 12:04', 0)
 
-SELECT * FROM Users
-
+--SELECT * FROM Users
