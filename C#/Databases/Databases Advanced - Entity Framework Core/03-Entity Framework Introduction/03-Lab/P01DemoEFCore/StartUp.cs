1b1eb15436f890188474f25ff6c3c2e9a956ae5b
Stefan Yankov <syankoff@gmail.com>
2022-06-23 20:25:45 +0300

    Add files via upload

---
+++ C#/Databases/Databases Advanced - Entity Framework Core/03-Entity Framework Introduction/03-Lab/P01DemoEFCore/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using P01DemoEFCore.Models;
+
+namespace P01DemoEFCore
+{
+    internal class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            // dotnet ef dbcontext scaffold "Server=.;Database=MusicX;Integrated Security=true" Microsoft.EntityFrameworkCore.SqlServer -o Models -f -d
+
+            var db = new MusicXContext();
+            Console.WriteLine(db.Songs.Count());
+            Console.WriteLine(db.Artists.Count());
+
+            var topArtistsBySongsCount = db.Artists
+                .OrderByDescending(x => x.SongArtists.Count())
+                .Select( x => new {x.Name, Count = x.SongArtists.Count()})
+                .Take(10).ToList();
+
+            foreach (var artist in topArtistsBySongsCount)
+            {
+                Console.WriteLine($"{artist.Name} {artist.Count}");
+            }
+
+        }
+    }
+}
